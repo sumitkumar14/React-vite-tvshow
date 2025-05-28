92dfc4d50d3222a8ab6a783154ca691e952222f4
Sumit Kumar <sumitkumar14695@gmail.com>
2025-05-28 10:42:48 +0530

    service file added

---
+++ src/services/apiService.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+// Define the base API URL
+const API_BASE_URL = "https://api.tvmaze.com";
+
+//header
+const headers = { "Content-Type": "application/json" };
+
+// Function to fetch TvSeriesList
+export const getSeriesList = async () => {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/shows`,{ headers });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    throw error; // Rethrow for handling in components
+  }
+};
+
+export const searchSeriesList = async (val) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/search/shows?q=${val}`,{ headers });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching searched posts:", error);
+      throw error; // Rethrow for handling in components
+    }
+  };
+
+  export const getSeriesDetails = async (showId) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/shows/${showId}`,{ headers });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      throw error; // Rethrow for handling in components
+    }
+  };
+
+  export const getSeriesCastDetails = async (showId) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/shows/${showId}/cast`,{ headers });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      throw error; // Rethrow for handling in components
+    }
+  };
+
+  export const getSeriesEpisodesDetails= async (showId) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/shows/${showId}/episodes`,{ headers });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      throw error; // Rethrow for handling in components
+    }
+  };
---
+++ src/tests/apiService.test.js
@@ -0,0 +1,54 @@
+import SeriesService from "@/services/apiService.js";
+import { expect, test } from 'vitest'
+
+describe('Service Class Testing', () => {
+    beforeEach(()=>{
+    })
+   test ('get api to get series list', async(done)=>{
+       try{
+        const SeriesService1 = new SeriesService();
+           const response=await SeriesService1.seriesList();
+           expect(response.data).toBeInstanceOf(Array);
+            //  done();
+       } catch(error){
+        //    done.fail(error)
+       }
+   })
+   test ('get api to get series list on search', async(done)=>{
+    try{
+     const SeriesService1 = new SeriesService();
+        const response=await SeriesService1.serachList('b');
+        expect(response.data[0]).toBeInstanceOf(Object)
+        //   done();
+    } catch(error){
+        // done.fail(error)
+    }
+})
+test ('api to get details of the series', async(done)=>{
+    try{
+     const SeriesService1 = new SeriesService();
+        const resp1=await SeriesService1.seriesDetails(211);
+        expect(resp1.data).toBeInstanceOf(Object)
+    } catch(error){
+        // done.fail(error)
+    }
+})
+test ('api to get series Casts details', async(done)=>{
+    try{
+     const SeriesService1 = new SeriesService();
+        const response=await SeriesService1.seriesCastDetails(211);
+        expect(response.data).toBeInstanceOf(Object)
+    } catch(error){
+        // done.fail(error)
+    }
+})
+test ('get api to get series cast details', async(done)=>{
+    try{
+     const SeriesService1 = new SeriesService();
+        const response=await SeriesService1.seriesEpisodesDetails(211);
+        expect(response.data).toBeInstanceOf(Object)
+    } catch(error){
+        // done.fail(error)
+    }
+})
+  })
